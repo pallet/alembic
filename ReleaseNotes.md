b65b865b633daff623410989bf75706c6eab9fa2
Hugo Duncan <hugo@hugoduncan.org>
2013-04-30 20:38:11 -0400

    Updated project.clj, release notes and readme for 0.1.1

--- ReleaseNotes.md
+++ ReleaseNotes.md
@@ -1,3 +1,9 @@
+# 0.1.1
+
+- Add clojure 1.4 dependency
+
+- Normalise :repositories to be a map
+
 # 0.1.0
 
 - Initial version
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject alembic "0.1.1-SNAPSHOT"
+(defproject alembic "0.1.1"
   :description "A jar distiller"
   :url "https://github.com/pallet/alembic"
   :license {:name "Eclipse Public License"
