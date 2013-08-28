cd52302dc3cbeaf28db57443155971cdf1f3f5aa
Hugo Duncan <hugo@hugoduncan.org>
2013-08-27 22:32:28 -0400

    Update to classlojure 0.7.0
    Removes transitive dependency on useful.

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [lein-as-resource "2.1.3"]
-                 [classlojure "0.6.6"]
+                 [org.flatland/classlojure "0.7.0"]
                  [org.tcrawley/dynapath "0.2.3"]])
