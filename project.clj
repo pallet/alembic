f8215c9558d43c0e0f9c2432c5dd21b96d985e12
Hugo Duncan <hugo@hugoduncan.org>
2013-08-28 22:08:10 -0400

    Updated version for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject alembic "0.2.0"
+(defproject alembic "0.2.1-SNAPSHOT"
   :description "A jar distiller"
   :url "https://github.com/pallet/alembic"
   :license {:name "Eclipse Public License"
