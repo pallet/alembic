d9685c8881c0689feb3a44e69dd56794280293d9
Hugo Duncan <hugo@hugoduncan.org>
2014-09-19 06:07:00 -0400

    Add clojure-1.6.0 profile

--- profiles.clj
+++ profiles.clj
@@ -15,4 +15,5 @@
  :no-checkouts {:checkout-shares ^:replace []} ; disable checkouts
  :clojure-1.4.0 {:dependencies [[org.clojure/clojure "1.4.0"]]}
  :clojure-1.5.0 {:dependencies [[org.clojure/clojure "1.5.0"]]}
- :clojure-1.5.1 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
+ :clojure-1.5.1 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+ :clojure-1.6.0 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
