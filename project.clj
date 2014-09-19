555245f46a4f9f0ac9df077fa2bd77e0ae900103
Hugo Duncan <hugo@hugoduncan.org>
2014-09-19 06:07:20 -0400

    Update to lein 2.5.0

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [lein-as-resource "2.1.3"]
+                 [lein-as-resource "2.5.0"]
                  [org.flatland/classlojure "0.7.0"]
                  [org.tcrawley/dynapath "0.2.3"]])
