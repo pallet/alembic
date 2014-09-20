3774cbd8046b9edba76c9fc231da5f3a298e7891
Hugo Duncan <hugo@hugoduncan.org>
2014-09-20 06:18:32 -0400

    Move clojure dependency to the :provided profile

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/pallet/alembic"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [lein-as-resource "2.5.0"]
+  :dependencies [[lein-as-resource "2.5.0"]
                  [org.flatland/classlojure "0.7.0"]
-                 [org.tcrawley/dynapath "0.2.3"]])
+                 [org.tcrawley/dynapath "0.2.3"]]
+  :profiles {:provided
+             {:dependencies [[org.clojure/clojure "1.4.0"]]}})
