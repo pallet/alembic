9af8c93301cf2458af081ba728027d8beafe9e23
redraiment <redraiment@gmail.com>
2016-08-27 21:51:42 +0800

    Update to lein-as-resource 2.7.0

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :url "https://github.com/pallet/alembic"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[lein-as-resource "2.5.0"]
+  :dependencies [[lein-as-resource "2.7.0"]
                  [org.flatland/classlojure "0.7.0"]
                  [org.tcrawley/dynapath "0.2.3"]]
   :exclusions [[org.clojure/clojure]]
