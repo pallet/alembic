95ae1c1713891e36adb10ced53ae595f75a3829e
Benedek Fazekas <benedek.fazekas@gmail.com>
2017-04-08 11:18:06 +0100

    Upgrade dynapath dependency
    mainly to fix java9 incompatibility. See
    clojure-emacs/refactor-nrepl#169 for details specially https://github.com/clojure-emacs/refactor-nrepl/issues/169#issuecomment-290243338

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[lein-as-resource "2.5.0"]
                  [org.flatland/classlojure "0.7.0"]
-                 [org.tcrawley/dynapath "0.2.3"]]
+                 [org.tcrawley/dynapath "0.2.5"]]
   :exclusions [[org.clojure/clojure]]
   :profiles {:provided
              {:dependencies [[org.clojure/clojure "1.4.0"]]}})
